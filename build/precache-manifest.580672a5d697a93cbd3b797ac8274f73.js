4bf0d319eddf107e6b761e186cbcae649e84d0cb
ambujdubey <ambujdubey89@gmail.com>
2020-10-21 07:22:35 +0530

    add app build

---
+++ build/precache-manifest.580672a5d697a93cbd3b797ac8274f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8014d372796cd642ece705553675061f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd2bcdfaae1f7a7a9993",
+    "url": "/static/css/main.c8f1ec50.chunk.css"
+  },
+  {
+    "revision": "65fdfa2fea4fd4023d82",
+    "url": "/static/js/2.a00f07b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a00f07b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2bcdfaae1f7a7a9993",
+    "url": "/static/js/main.72900fab.chunk.js"
+  },
+  {
+    "revision": "d141909a6584a1b2eb4b",
+    "url": "/static/js/runtime-main.fd808a83.js"
+  }
+]);
